af551e78a361ae59564bcccffa05ecdedad10a74
Marco Kähler <m.kaehler@internetagentur-kaehler.de>
2022-02-08 09:15:49 +0100

    asset prefix fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "custom",
     domains: ['marcokaehler.dev', 'tribari.github.io'],
-  },
-  assetPrefix: isProd ? '/marcokaehler.dev/' : ''
+  }
 }
 
 module.exports = nextConfig
